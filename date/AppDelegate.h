a825ab4f0dc72151b06b66805f3bc647c6d08a2d
郭嘉 <guojia@guojiadeMacBook-Pro.local>
2015-09-08 00:26:51 +0800

    Initial Commit

---
+++ date/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  date
+//
+//  Created by 郭嘉 on 15/9/8.
+//  Copyright (c) 2015年 郭嘉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ date/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  date
+//
+//  Created by 郭嘉 on 15/9/8.
+//  Copyright (c) 2015年 郭嘉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
